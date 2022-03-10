9dfa6863cdf35ad495d442626f89d6312c5b1f4a
Frizlab <fload@me.com>
2022-03-10 23:31:34 +0100

    Use new path for happn dependencies

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.4.0"),
-		.package(url: "https://github.com/happn-tech/XibLoc.git", from: "1.1.1"),
+		.package(url: "https://github.com/happn-app/XibLoc.git", from: "1.1.1"),
 		.package(url: "https://github.com/xcode-actions/clt-logger.git", from: "0.3.4"),
 		.package(url: "https://github.com/xcode-actions/swift-signal-handling.git", from: "0.2.0"),
 		.package(url: "https://github.com/xcode-actions/XcodeTools.git", from: "0.3.5")
