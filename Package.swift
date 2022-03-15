bf65dc8a34c521fe92753828469458cd6424a0f9
Frizlab <fload@me.com>
2022-03-15 23:53:02 +0100

    OpenSSL 1.1.1n

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
 		.library(name: "COpenSSL-dynamic", targets: ["COpenSSL-dynamic"])
 	],
 	targets: [
-//		.binaryTarget(name: "COpenSSL-static", url: "https://github.com/xcode-actions/COpenSSL/releases/download/1.1.111/COpenSSL-static.xcframework.zip", checksum: "f93578dd55004b8454dc771166a911e82f91e98aad636f9d1556b67a02ee5a48"),
-		.binaryTarget(name: "COpenSSL-dynamic", url: "https://github.com/xcode-actions/COpenSSL/releases/download/1.1.111/COpenSSL-dynamic.xcframework.zip", checksum: "18a82ef2a2188955e99f0089572760c7f70cb9b2d79ccf928fe5464885af8703")
+		.binaryTarget(name: "COpenSSL-static", url: "https://github.com/xcode-actions/COpenSSL/releases/download/1.1.114/COpenSSL-static.xcframework.zip", checksum: "20d5b0c3ca31a9bad325208e42ee8aeac729f1ba5d4067ec07bb92beff5761f6"),
+		.binaryTarget(name: "COpenSSL-dynamic", url: "https://github.com/xcode-actions/COpenSSL/releases/download/1.1.114/COpenSSL-dynamic.xcframework.zip", checksum: "1ff09aaa3225e3aec3efe541a874c8b1907d828f6ed65257c905a404bfc655b5")
 	]
 )
